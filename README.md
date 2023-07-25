51c603694a46979d1969d7ba7e936181b54c4297
shahzain46 <shahzain6016@gmail.com>
2023-07-25 12:00:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+ portfolio website with different themes
---
+++ js/script.js
@@ -0,0 +1,8 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings: [ "Mern Stack Developer",],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
